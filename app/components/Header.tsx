20276944dd6f571c453fa391b28d0a7917c701ce
Mees <meesboers@hotmail.com>
2025-10-19 18:21:47 +0200

    solved build errors

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,12 @@
 
 import KlatturLogo from "@/public/KlatturLogo";
+import Link from "next/link";
 async function Header() {
   
   return (
     <div className="h-[80px] bg-white sticky top-0 flex items-center flex-shrink-0 z-20">
       <div className="flex justify-between">
-          <a href="/" className="w-[100px]"><KlatturLogo/></a>
+          <Link href="/" className="w-[100px]"><KlatturLogo/></Link>
       </div>
     </div>
      
--- public/KlatturLogo.tsx
+++ public/KlatturLogo.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-const KlatturLogo = (props) => (
+const KlatturLogo = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlSpace="preserve"
