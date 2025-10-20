6bbe4c5d310794c9172019946b8a76ffb725d988
Mees <meesboers@hotmail.com>
2025-10-20 16:54:16 +0200

    added an xs media size

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ function page() {
       </div>
       <div className="flex flex-col  w-[50%] justify-center items-start">
         <div className="flex flex-col items-center ml-[10px] mb-[11px] sm:mb-[14px] sm:ml-[18px] md:mb-[20px] md:ml-[30px] lg:mb-[30px] lg:ml-[40px] xl:mb-[38px] xl:ml-[50px]">
-          <h1 className="font-no-name-regular text-blue text-[28px] py-[8px] leading-tight sm:text-[38px] md:text-[48px] md:py-[14px] lg:text-[64px] xl:text-[83px] xl:py-[22px]">
+          <h1 className="font-no-name-regular text-blue text-[18px] py-[8px] xs:text-[28px] leading-tight sm:text-[38px] md:text-[48px] md:py-[14px] lg:text-[64px] xl:text-[83px] xl:py-[22px]">
             Clear your <br /> mind clutter <br /> and get rest
           </h1>
           <a href="/boek" className="bg-logo font-open-sans-semibold text-dark-blue text-center text-[14px] rounded-[2000px] w-[150px] py-[4px] px-[15px] md:text-[20px] md:w-[200px]  xl:text-[30px] xl:w-[320px]">
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
+    screens: {
+      'xs': '320px',  // or whatever size you prefer
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         nonamreg: ['var(--font-no-name-regular)'],
