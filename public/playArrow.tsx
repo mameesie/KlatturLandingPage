44bc1ea45c6cbdd75da97547d30fa23737cc7ae4
Mees <meesboers@hotmail.com>
2026-06-11 10:05:19 +0200

    TEST if new svg is on session button on safari mobile

--- public/playArrow.tsx
+++ public/playArrow.tsx
@@ -1,26 +1,12 @@
 import * as React from "react"
 const PlayArrow = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlSpace="preserve"
-    style={{
-      fillRule: "evenodd",
-      clipRule: "evenodd",
-      strokeLinecap: "round",
-      strokeLinejoin: "round",
-      strokeMiterlimit: 1.5,
-    }}
-    viewBox="0 0 47 62"
-    {...props}
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47 62" {...props}>
     <path
-      d="m1108.488 379.35 30.883 42.448h-61.766l30.883-42.448Z"
-      style={{
-        fill: "#fff",
-        stroke: "#fff",
-        strokeWidth: "9.58px",
-      }}
-      transform="rotate(90 560.085 -237.353) scale(.74773)"
+      d="M 9 6 L 42 31 L 9 56 Z"
+      fill="#fff"
+      stroke="#fff"
+      strokeWidth="4"
+      strokeLinejoin="round"
     />
   </svg>
 )
